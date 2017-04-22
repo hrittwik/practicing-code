c1584382d2271d46ccac1a570b0e00e8ef5f1c75
hrittwik <hrittwik@gmail.com>
2017-04-22 22:10:37 +0600

    edit

--- Python/Learn_Python_The_Hard_Way/ex12.py
+++
@@ -1,7 +0,0 @@
-name = raw_input("Name? ") 
-age = raw_input("how old are you?")
-height = raw_input("How tall are you? ")
-weight = raw_input("How much do you weight? ")
- 
-print "So, you're %r and %r years old, %r tall and %r heavy." % (name,age,height,weight) 
-
--- Python/Learn_Python_The_Hard_Way/ex13.py
+++
@@ -1,9 +0,0 @@
-from sys import argv 
-
-script, first, second, third = argv
-
-print "This script is called: ", script
-print "Your first variable is: ", first
-print "Your second variavle is: ", second
-print "Your third variable is: ", third
-
