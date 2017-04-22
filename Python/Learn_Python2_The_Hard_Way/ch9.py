970e077703bbd3a7cc815cf63f646f6ca0d27001
hrittwik <hrittwik@gmail.com>
2017-04-22 22:13:57 +0600

    edit

---
+++ Python/Learn_Python2_The_Hard_Way/ch9.py
@@ -0,0 +1,7 @@
+#Here are some new strange stuff, remember type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJune\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print "Here are the days: ", days
+print "Here are the months: ",  months
---
+++ Python/Learn_Python2_The_Hard_Way/ex12.py
@@ -0,0 +1,7 @@
+name = raw_input("Name? ") 
+age = raw_input("how old are you?")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weight? ")
+ 
+print "So, you're %r and %r years old, %r tall and %r heavy." % (name,age,height,weight) 
+
